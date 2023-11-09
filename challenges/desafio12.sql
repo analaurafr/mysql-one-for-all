f7720c3f80f333e1d222b808b5dcf0eab21dc8fe
analaurafr <laurafernandes@outlook.com>
2023-11-09 20:50:39 -0300

    refatorando requisito 12

--- challenges/desafio12.sql
+++ challenges/desafio12.sql
@@ -12,4 +12,12 @@ LEFT JOIN albuns al ON a.artista_id = al.artista_id
 LEFT JOIN musicas m ON al.album_id = m.album_id
 LEFT JOIN musicas_favoritas mf ON m.musica_id = mf.musica_id
 GROUP BY artista
-ORDER BY ranking DESC, artista;
+ORDER BY
+  CASE
+    WHEN COUNT(mf.musica_id) >= 5 THEN 1
+    WHEN COUNT(mf.musica_id) BETWEEN 3 AND 4 THEN 2
+    WHEN COUNT(mf.musica_id) BETWEEN 1 AND 2 THEN 3
+    ELSE 4
+  END,
+  artista;
+
